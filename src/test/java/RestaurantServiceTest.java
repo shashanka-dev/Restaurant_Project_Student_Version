2634400bd3327149dfef53bb907ac684bda4f2c9
Shashanka Shekhar <shashanka1995@users.noreply.github.com>
2021-04-05 23:08:37 +0530

    Part 2: Solution

--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.*;
 
 import java.time.LocalTime;
-import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
